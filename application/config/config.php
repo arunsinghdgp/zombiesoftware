9d9499d906c6002c04998e2f87ed44e739176d3e
Arun Kumar Singh <arunsingh.dgp@gmail.com>
2017-06-16 15:12:16 +0530

    config changes
    config changes

--- application/config/config.php
+++ application/config/config.php
@@ -23,6 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
+$config['base_url'] = 'http://139.162.176.174/';
 
 /*
 |--------------------------------------------------------------------------
